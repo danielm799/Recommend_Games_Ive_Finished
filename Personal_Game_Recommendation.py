4a2868b6c8a1521e1429e729cb950df66d08a659
Daniel Medina <dmedina799@gmail.comgit config --global user.email>
2022-03-23 09:14:19 -0400

    Im adding a python file to the repository

---
+++ Personal_Game_Recommendation.py
@@ -0,0 +1,23 @@
+#create a class that will have the functionality of a graph and a linked list
+
+#use the premade list of titles and make all of the genre objects
+
+#use that same list to make the title objects
+
+#somehow make a for loop that takes every title's list of relevant genres to make an edge 
+#between the title and the relevant genres, or make a function do it
+
+#also set up a linked list that connects titles to each other as to prevent the need of making a list of titles.
+#saves memory 
+
+#now ask the user if they want to search through titles or by genre
+
+#if statement for their choice and then ask for a few characters that hopefully match something in this program
+
+#if title, set pointers at the head and tail node to hopefully speed up the search for a relevant title. Relevant 
+#titles will be added to a list (the object themselves). The list is produced and I somehow need to proint out the
+#string Titles and their Genre lists.
+
+#its easier with Genres. I just use a bunch of if statements and do something similar with Titles like adding 
+#relevant genres to a list and printing them out in the end, but I'll be printing out the edges too.
+
